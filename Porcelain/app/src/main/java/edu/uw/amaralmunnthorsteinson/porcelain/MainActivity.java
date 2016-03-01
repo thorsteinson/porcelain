85f01d5f80aef3862420fc6f894fe9c37ff288de
Caleb Thorsteinson <caleb@thorsteinson.io>
2016-03-01 15:05:21 -0800

    Added firebase dependency

--- Porcelain/app/src/main/java/edu/uw/amaralmunnthorsteinson/porcelain/MainActivity.java
+++ Porcelain/app/src/main/java/edu/uw/amaralmunnthorsteinson/porcelain/MainActivity.java
@@ -3,11 +3,16 @@ package edu.uw.amaralmunnthorsteinson.porcelain;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Firebase setup
+        Firebase.setAndroidContext(this);
     }
 }
