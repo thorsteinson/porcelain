aec4a876596f8c8ea86778c0b98f399cd0e29ff8
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-03-09 16:39:39 -0800

    Wired listeners up to graphical components
    Now our class has instance variables that are tied to actual interface
    components that are in the layout. It also prevents the FAB from appearing
    while the required attributes don't have any values. This makes it so that
    only clean data can be added to firebase. Now we just have to actually use
    firebase to push the data out.

--- Porcelain/app/src/main/java/edu/uw/amaralmunnthorsteinson/porcelain/AddToiletActivity.java
+++ Porcelain/app/src/main/java/edu/uw/amaralmunnthorsteinson/porcelain/AddToiletActivity.java
@@ -2,9 +2,19 @@ package edu.uw.amaralmunnthorsteinson.porcelain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class AddToiletActivity extends AppCompatActivity {
 
@@ -15,6 +25,16 @@ public class AddToiletActivity extends AppCompatActivity {
     private Double latitude;
     private Double longitude;
 
+    // Instance variables that come from our GUI
+    // Optional
+    public boolean mGenderNeutral = false;
+    public boolean mFamilyFriendly = false;
+    public boolean mHandicapAccessible = false;
+    public String mNotes = "";
+    // Required
+    public Long mRating = null;
+    public String mName = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,27 +44,101 @@ public class AddToiletActivity extends AppCompatActivity {
         longitude = getIntent().getDoubleExtra(MainActivity.LONGITUDE, 0);
 
         getSupportActionBar().setTitle("Add New Toilet");
+        registerListeners();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    // Registers all of the callbacks for our interface so we modify the proper instance variables
+    public void registerListeners() {
+        // Set up listener for the name
+        EditText nameEditText = (EditText) findViewById(R.id.add_toilet_name);
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        // Make our add button visible
-        View addButton = findViewById(R.id.add_toilet_add_button);
-        addButton.setVisibility(View.VISIBLE);
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Update the name
+                mName = s.toString();
+                updateAddButtonVisibility();
+            }
+        });
+
+        // Set up listener for the notes
+        EditText notesEditText = (EditText) findViewById(R.id.add_toilet_notes);
+        notesEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Update the name
+                mNotes = s.toString();
+                updateAddButtonVisibility();
+            }
+        });
+
+        // Set up listener for the rating
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.add_toilet_cleanliness_rating);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                mRating = ((Float) rating).longValue();
+                updateAddButtonVisibility();
+            }
+        });
+
+        // Set up listeners for the amenities
+        Switch genderNeutralSwitch = (Switch) findViewById(R.id.add_toilet_is_gender_neutral);
+        genderNeutralSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mGenderNeutral = isChecked;
+            }
+        });
+
+        Switch handicapAccessibleSwitch = (Switch) findViewById(R.id.add_toilet_is_handicap_accessible);
+        handicapAccessibleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mHandicapAccessible = isChecked;
+            }
+        });
+
+        Switch familyFriendlySwitch = (Switch) findViewById(R.id.add_toilet_is_family_friendly);
+        familyFriendlySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mFamilyFriendly = isChecked;
+            }
+        });
+    }
+
+    // Makes the addButton visible if all of the required attributes have been set
+    public void updateAddButtonVisibility() {
+        if (mName != null && mRating != null) {
+            findViewById(R.id.add_toilet_add_button).setVisibility(View.VISIBLE);
+        }
     }
 
     // This method when called will take all of the data in the various forms and submit it
     // to firebase. If everything goes successfully, it will should end the activity and give a
     // happy message
     public void addToiletToFirebase(View v) {
-        Log.v(TAG, "We've clicked ZE BUTTON");
         // Look up all the data and make sure the forms are completed before continuing
 
         // Make a call to firebase and actually add our new toilet
 
         // Actually ends the activity
-        // finish();
+         finish();
     }
 }
